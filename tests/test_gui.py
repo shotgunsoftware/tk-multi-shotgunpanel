6eae052a99cb1d992ffed2529e31233bde8bf714
Stacey Oue <stacey.oue@autodesk.com>
2022-01-13 09:40:31 -0500

    Revert change to validate unit tests still pass.

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -99,7 +99,7 @@ class AppDialogAppWrapper(object):
         """
         :param root:
         """
-        self.root = parent["ShotGrid: ShotGrid Panel"].get()
+        self.root = parent["ShotGrid: ShotGrid"].get()
 
     def exists(self):
         """
