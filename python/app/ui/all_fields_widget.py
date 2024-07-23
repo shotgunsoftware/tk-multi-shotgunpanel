f5770c809a565663ca4c3314d95933f75573658b
Julien Langlois <julien.langlois@autodesk.com>
2024-07-23 17:26:55 +0200

    Apply script - TODO CHECK THAT!

--- python/app/ui/all_fields_widget.py
+++ python/app/ui/all_fields_widget.py
@@ -1,37 +1,50 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'all_fields_widget.ui'
-#
-#      by: pyside-uic 0.2.15 running on PySide 1.2.2
-#
-# WARNING! All changes made in this file will be lost!
+################################################################################
+## Form generated from reading UI file 'all_fields_widget.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.16
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
 
-from tank.platform.qt import QtCore, QtGui
+from . import QtCore
+for name, cls in QtCore.__dict__.items():
+    if isinstance(cls, type): globals()[name] = cls
+
+from . import QtGui
+for name, cls in QtGui.__dict__.items():
+    if isinstance(cls, type): globals()[name] = cls
+
 
 class Ui_AllFieldsWidget(object):
     def setupUi(self, AllFieldsWidget):
-        AllFieldsWidget.setObjectName("AllFieldsWidget")
+        if not AllFieldsWidget.objectName():
+            AllFieldsWidget.setObjectName(u"AllFieldsWidget")
         AllFieldsWidget.resize(337, 645)
-        self.verticalLayout = QtGui.QVBoxLayout(AllFieldsWidget)
+        self.verticalLayout = QVBoxLayout(AllFieldsWidget)
         self.verticalLayout.setSpacing(0)
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
-        self.verticalLayout.setObjectName("verticalLayout")
-        self.all_fields_scroll_area = QtGui.QScrollArea(AllFieldsWidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.all_fields_scroll_area = QScrollArea(AllFieldsWidget)
+        self.all_fields_scroll_area.setObjectName(u"all_fields_scroll_area")
         self.all_fields_scroll_area.setWidgetResizable(True)
-        self.all_fields_scroll_area.setObjectName("all_fields_scroll_area")
-        self.all_fields_host = QtGui.QWidget()
-        self.all_fields_host.setGeometry(QtCore.QRect(0, 0, 335, 643))
-        self.all_fields_host.setObjectName("all_fields_host")
-        self.all_fields_layout = QtGui.QGridLayout(self.all_fields_host)
-        self.all_fields_layout.setContentsMargins(0, 0, 0, 0)
+        self.all_fields_host = QWidget()
+        self.all_fields_host.setObjectName(u"all_fields_host")
+        self.all_fields_host.setGeometry(QRect(0, 0, 335, 643))
+        self.all_fields_layout = QGridLayout(self.all_fields_host)
         self.all_fields_layout.setSpacing(0)
-        self.all_fields_layout.setObjectName("all_fields_layout")
+        self.all_fields_layout.setContentsMargins(0, 0, 0, 0)
+        self.all_fields_layout.setObjectName(u"all_fields_layout")
         self.all_fields_scroll_area.setWidget(self.all_fields_host)
+
         self.verticalLayout.addWidget(self.all_fields_scroll_area)
 
         self.retranslateUi(AllFieldsWidget)
-        QtCore.QMetaObject.connectSlotsByName(AllFieldsWidget)
+
+        QMetaObject.connectSlotsByName(AllFieldsWidget)
+    # setupUi
 
     def retranslateUi(self, AllFieldsWidget):
-        AllFieldsWidget.setWindowTitle(QtGui.QApplication.translate("AllFieldsWidget", "Form", None, QtGui.QApplication.UnicodeUTF8))
-
+        AllFieldsWidget.setWindowTitle(QCoreApplication.translate("AllFieldsWidget", u"Form", None))
+    # retranslateUi
--- python/app/ui/list_item_widget.py
+++ python/app/ui/list_item_widget.py
@@ -1,77 +1,102 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'list_item_widget.ui'
-#
-#      by: pyside-uic 0.2.15 running on PySide 1.2.2
-#
-# WARNING! All changes made in this file will be lost!
+################################################################################
+## Form generated from reading UI file 'list_item_widget.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.16
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
 
-from tank.platform.qt import QtCore, QtGui
+from . import QtCore
+for name, cls in QtCore.__dict__.items():
+    if isinstance(cls, type): globals()[name] = cls
+
+from . import QtGui
+for name, cls in QtGui.__dict__.items():
+    if isinstance(cls, type): globals()[name] = cls
+
+
+from  . import resources_rc
 
 class Ui_ListItemWidget(object):
     def setupUi(self, ListItemWidget):
-        ListItemWidget.setObjectName("ListItemWidget")
+        if not ListItemWidget.objectName():
+            ListItemWidget.setObjectName(u"ListItemWidget")
         ListItemWidget.resize(355, 105)
-        self.horizontalLayout_3 = QtGui.QHBoxLayout(ListItemWidget)
+        self.horizontalLayout_3 = QHBoxLayout(ListItemWidget)
         self.horizontalLayout_3.setSpacing(1)
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
         self.horizontalLayout_3.setContentsMargins(8, 4, 8, 4)
-        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
-        self.box = QtGui.QFrame(ListItemWidget)
-        self.box.setFrameShape(QtGui.QFrame.NoFrame)
-        self.box.setObjectName("box")
-        self.horizontalLayout_2 = QtGui.QHBoxLayout(self.box)
+        self.box = QFrame(ListItemWidget)
+        self.box.setObjectName(u"box")
+        self.box.setFrameShape(QFrame.NoFrame)
+        self.horizontalLayout_2 = QHBoxLayout(self.box)
         self.horizontalLayout_2.setSpacing(10)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
         self.horizontalLayout_2.setContentsMargins(4, 8, 4, 8)
-        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
-        self.thumbnail = QtGui.QLabel(self.box)
-        self.thumbnail.setMinimumSize(QtCore.QSize(96, 75))
-        self.thumbnail.setMaximumSize(QtCore.QSize(96, 75))
-        self.thumbnail.setText("")
-        self.thumbnail.setPixmap(QtGui.QPixmap(":/tk_multi_infopanel/rect_512x400.png"))
+        self.thumbnail = QLabel(self.box)
+        self.thumbnail.setObjectName(u"thumbnail")
+        self.thumbnail.setMinimumSize(QSize(96, 75))
+        self.thumbnail.setMaximumSize(QSize(96, 75))
+        self.thumbnail.setPixmap(QPixmap(u":/tk_multi_infopanel/rect_512x400.png"))
         self.thumbnail.setScaledContents(True)
-        self.thumbnail.setAlignment(QtCore.Qt.AlignCenter)
-        self.thumbnail.setObjectName("thumbnail")
+        self.thumbnail.setAlignment(Qt.AlignCenter)
+
         self.horizontalLayout_2.addWidget(self.thumbnail)
-        self.verticalLayout = QtGui.QVBoxLayout()
+
+        self.verticalLayout = QVBoxLayout()
         self.verticalLayout.setSpacing(3)
-        self.verticalLayout.setObjectName("verticalLayout")
-        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setSpacing(5)
-        self.horizontalLayout.setObjectName("horizontalLayout")
-        self.list_item_top_left = QtGui.QLabel(self.box)
-        self.list_item_top_left.setObjectName("list_item_top_left")
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.list_item_top_left = QLabel(self.box)
+        self.list_item_top_left.setObjectName(u"list_item_top_left")
+
         self.horizontalLayout.addWidget(self.list_item_top_left)
-        self.list_item_top_right = QtGui.QLabel(self.box)
-        self.list_item_top_right.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
-        self.list_item_top_right.setObjectName("list_item_top_right")
+
+        self.list_item_top_right = QLabel(self.box)
+        self.list_item_top_right.setObjectName(u"list_item_top_right")
+        self.list_item_top_right.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+
         self.horizontalLayout.addWidget(self.list_item_top_right)
-        self.button = QtGui.QPushButton(self.box)
-        self.button.setMaximumSize(QtCore.QSize(16, 16))
-        self.button.setIconSize(QtCore.QSize(10, 10))
-        self.button.setObjectName("button")
+
+        self.button = QPushButton(self.box)
+        self.button.setObjectName(u"button")
+        self.button.setMaximumSize(QSize(16, 16))
+        self.button.setIconSize(QSize(10, 10))
+
         self.horizontalLayout.addWidget(self.button)
+
         self.verticalLayout.addLayout(self.horizontalLayout)
-        self.list_item_body = QtGui.QLabel(self.box)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Expanding)
+
+        self.list_item_body = QLabel(self.box)
+        self.list_item_body.setObjectName(u"list_item_body")
+        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.list_item_body.sizePolicy().hasHeightForWidth())
         self.list_item_body.setSizePolicy(sizePolicy)
-        self.list_item_body.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+        self.list_item_body.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignTop)
         self.list_item_body.setWordWrap(True)
-        self.list_item_body.setObjectName("list_item_body")
+
         self.verticalLayout.addWidget(self.list_item_body)
+
         self.horizontalLayout_2.addLayout(self.verticalLayout)
+
         self.horizontalLayout_3.addWidget(self.box)
 
         self.retranslateUi(ListItemWidget)
-        QtCore.QMetaObject.connectSlotsByName(ListItemWidget)
+
+        QMetaObject.connectSlotsByName(ListItemWidget)
+    # setupUi
 
     def retranslateUi(self, ListItemWidget):
-        ListItemWidget.setWindowTitle(QtGui.QApplication.translate("ListItemWidget", "Form", None, QtGui.QApplication.UnicodeUTF8))
-        self.list_item_top_left.setText(QtGui.QApplication.translate("ListItemWidget", "Hello World", None, QtGui.QApplication.UnicodeUTF8))
-        self.list_item_top_right.setText(QtGui.QApplication.translate("ListItemWidget", "3 days ago", None, QtGui.QApplication.UnicodeUTF8))
-        self.list_item_body.setText(QtGui.QApplication.translate("ListItemWidget", "Body text\n"
-"hello", None, QtGui.QApplication.UnicodeUTF8))
-
-from . import resources_rc
+        ListItemWidget.setWindowTitle(QCoreApplication.translate("ListItemWidget", u"Form", None))
+        self.thumbnail.setText("")
+        self.list_item_top_left.setText(QCoreApplication.translate("ListItemWidget", u"Hello World", None))
+        self.list_item_top_right.setText(QCoreApplication.translate("ListItemWidget", u"3 days ago", None))
+        self.list_item_body.setText(QCoreApplication.translate("ListItemWidget", u"Body text\n"
+"hello", None))
+    # retranslateUi
